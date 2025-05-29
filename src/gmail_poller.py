b8a8f421d0bfc7fd8067661adb877e51320f236f
yagnikposhiya <yagnikposhiya97275@gmail.com>
2025-05-29 18:29:10 +0530

    Add if condition for empty emails list

--- src/gmail_poller.py
+++ src/gmail_poller.py
@@ -11,14 +11,15 @@ from storage.dynamodb_handler import store_email_log
 emails = fetch_unread_emails() # call the function to get a list of unread emails (each as a dictionary
 
 # iterate through each mail and display its details
-for mail in emails:
-    # print(f"From_name: {mail['from_name']}")
-    # print(f"From_email: {mail['from_email']}")
-    # print(f"To: {mail['to']}")
-    # print(f"Date: {mail['date']}")
-    # print(f"Time: {mail['time']}")
-    # print(f"Subject: {mail['subject']}")
-    # print(f"Body:\n{mail['body']}")
+if emails:
+    for mail in emails:
+        # print(f"From_name: {mail['from_name']}")
+        # print(f"From_email: {mail['from_email']}")
+        # print(f"To: {mail['to']}")
+        # print(f"Date: {mail['date']}")
+        # print(f"Time: {mail['time']}")
+        # print(f"Subject: {mail['subject']}")
+        # print(f"Body:\n{mail['body']}")
 
-    store_email_log(mail) # save email metadata with body content
-    print(f"Stored email from: {mail['from_email']}")
+        store_email_log(mail) # save email metadata with body content
+        print(f"Stored email from: {mail['from_email']}")
